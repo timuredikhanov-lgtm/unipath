b262b4ac711126662906c19c506ea494ab28ac0a
Rishat Edikhanov <rishatedikhanov@MacBook-Pro-Rishat.local>
2026-07-03 15:47:14 +0300

    fix: simplify prisma client init

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,15 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// На serverless (Vercel) переиспользуем экземпляр через globalThis
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 globalForPrisma.prisma = prisma;
